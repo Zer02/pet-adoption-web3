ff5ff82988b5123e5e2b831ef59d1fbcc257cce7
Anil <zero@EVE.fios-router.home>
2024-01-07 17:08:27 -0500

    initial commit

---
+++ test/PetAdoption.js
@@ -0,0 +1,32 @@
+
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { expect } = require("chai");
+
+describe("PetAdoption", function () {
+
+	async function deployContractFixture() {
+		const [owner] = await ethers.getSigners();
+		const PetAdoption = await ethers.getContractFactory("PetAdoption");
+		const contract = await PetAdoption.deploy();
+
+		return { owner, contract };
+	}
+
+	describe("Deployment", function () {
+		it("Should set the right owner", async function () {
+			const { owner, contract } = await loadFixture(deployContractFixture);
+			const contractOwner = await contract.owner();
+			expect(contractOwner).to.equal(owner.address);
+		});
+
+		it("Should return the right owner", async function () {
+			const { owner, contract } = await loadFixture(deployContractFixture);
+			const contractOwner = await contract.getOwner();
+			expect(contractOwner).to.equal(owner.address);
+		});
+	});
+});
+
+
+
+// npx hardhat test --network hardhat

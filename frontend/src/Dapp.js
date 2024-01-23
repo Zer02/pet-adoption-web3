4c602c12e0003e3ddb489ccbd5e872f8df5291cb
Anil <abasdeo29@gmail.com>
2024-01-22 21:10:23 -0500

    added function: Connect Wallet

--- frontend/src/Dapp.js
+++ frontend/src/Dapp.js
@@ -4,9 +4,11 @@ import { Navbar } from "./components/Navbar";
 import { PetItem } from "./components/PetItem";
 import { TxError } from "./components/TxError";
 import { WalletNotDetected } from "./components/WalletNotDetected";
+import { ConnectWallet } from "./components/ConnectWallet";
 
 function Dapp() {
   const [pets, setPets] = useState([]);
+  const [selectedAddress, setSelectedAddress] = useState(undefined);
 
   useEffect(() => {
     async function fetchPets() {
@@ -18,15 +20,29 @@ function Dapp() {
     fetchPets();
   }, []);
 
-  if(window.ethereum === undefined) {
+  async function connectWallet() {
+    try {
+      const [address] = await window.ethereum.request({method: "eth_requestAccounts"});
+      setSelectedAddress(address);
+
+    } catch(e) {
+      console.error(e.message);
+    }
+  }
+
+  if(!window.ethereum) {
     return <WalletNotDetected />
+  }
+
+  if (!selectedAddress) {
+    return <ConnectWallet connect={connectWallet} />
   }
 
   return (
     <div className="container">
       <TxError />
       <br />
-      <Navbar />
+      <Navbar address={selectedAddress} />
       <div className="items">
         { pets.map((pet) => 
           <PetItem key={pet.id} pet={pet} />
